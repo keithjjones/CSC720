26ceb15724269c3d010f937d4f9db408e8274e7c
Keith J. Jones <keith@keithjjones.com>
2018-03-31 18:46:58 -0400

    Finished functionality.

--- Class_Project/compiler.py
+++ Class_Project/compiler.py
@@ -6,6 +6,12 @@ import ply.yacc as yacc
 # Get the token map from the lexer.  This is required.
 from lexer import tokens
 
+import llvmlite.ir as ll
+import llvmlite.binding as llvm
+
+llvm.initialize()
+llvm.initialize_native_target()
+llvm.initialize_native_asmprinter()
 
 precedence = (
     ('left', 'PLUS', 'MINUS'),
@@ -64,41 +70,74 @@ print("Using Data: {0}".format(input_string))
 result = parser.parse(input_string)
 print("YACC/AST Results: {0}".format(result))
 
+module = ll.Module()
+func_ty = ll.FunctionType(ll.IntType(64), [])
+func = ll.Function(module, func_ty, name='main')
 
-def crawl_ast(node):
+bb_entry = func.append_basic_block('entry')
+irbuilder = ll.IRBuilder(bb_entry)
+
+# tmp = irbuilder.add(func.args[0], func.args[1])
+# ret = irbuilder.ret(tmp)
+
+
+def crawl_ast(node, irb):
     # print("Node: {0}".format(node))
     results = []
     # Crawl each branch...
     for i in range(1, len(node)):
         if type(node[i]) is tuple:
-            result = crawl_ast(node[i])
+            result = crawl_ast(node[i], irb)
             output = result
             results.append(result)
         else:
-            output = node[i]
+            output = ll.Constant(ll.IntType(64), node[i])
 
     # Now do the math...
     if node[0] == "TIMES":
         for i in range(1, len(results)):
-            output = results[i-1] * results[i]
+            output = irb.mul(results[i-1], results[i])
         # print("Mult: {0}".format(output))
     elif node[0] == "DIVIDE":
         for i in range(1, len(results)):
-            output = results[i-1] / results[i]
+            output = irb.udiv(results[i-1], results[i])
         # print("Div: {0}".format(output))
     elif node[0] == "PLUS":
         for i in range(1, len(results)):
-            output = results[i-1] + results[i]
+            output = irb.add(results[i-1], results[i])
         # print("Add: {0}".format(output))
     elif node[0] == "MINUS":
         for i in range(1, len(results)):
-            output = results[i-1] - results[i]
+            output = irb.sub(results[i - 1], results[i])
         # print("Sub: {0}".format(output))
 
     # print("Output: {0}".format(output))
     return output
 
 
-result = crawl_ast(result)
+result = crawl_ast(result, irbuilder)
+irbuilder.ret(result)
+
 print()
-print("Calculated Result: {0}".format(result))
+print('=== Begin LLVM IR')
+print(module)
+print('=== End LLVM IR')
+
+# target = llvm.Target.from_triple(llvm.get_default_triple())
+target = llvm.Target.from_default_triple()
+target_machine = target.create_target_machine()
+backing_mod = llvm.parse_assembly("")
+engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
+
+mod = llvm.parse_assembly(str(module))
+mod.verify()
+# Now add the module and make sure it is ready for execution
+engine.add_module(mod)
+engine.finalize_object()
+# engine.run_static_constructors()
+
+engine.finalize_object()
+print('=== Begin Assembly')
+print(target_machine.emit_assembly(mod))
+print('=== End Assembly')
+print("Target machine: {0}".format(llvm.get_default_triple()))
